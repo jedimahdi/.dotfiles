d0c1c53de3b14b8b57cdaaa01fdbe12dd4564230
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2022-10-06 21:36:41 +0330

    add purescript nix template

---
+++ templates/purescript/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Node environment";
+
+  inputs = {
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-22.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    easy-purescript-src = {
+      url = "github:justinwoo/easy-purescript-nix";
+      flake = false;
+    };
+  };
+
+  outputs = { utils, nixpkgs-stable, nixpkgs-unstable, easy-purescript-src, ... }:
+    let name = "purescript";
+    in
+    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+      let
+        easy-purescript = import easy-purescript-src { pkgs = pkgs.stable; };
+        pkgs = {
+          stable = import nixpkgs-stable {
+            inherit system;
+            overlays = [ (_: _: { inherit (easy-purescript) purs-tidy pulp; purescript = easy-purescript.purs-0_14_5; }) ];
+          };
+          unstable = import nixpkgs-unstable { inherit system; };
+        };
+      in
+      {
+        devShell = import ./shell.nix { inherit name pkgs; };
+      });
+}
---
+++ templates/purescript/shell.nix
@@ -0,0 +1,49 @@
+{ pkgs, name }:
+
+pkgs.stable.mkShell {
+  inherit name;
+
+  nativeBuildInputs = builtins.concatMap builtins.attrValues [
+    ###################################################
+    # Code styles:
+    {
+      inherit (pkgs.stable) pre-commit nixpkgs-fmt nix-linter purs-tidy shfmt;
+      inherit (pkgs.unstable.python3Packages) pre-commit-hooks yamllint;
+      inherit (pkgs.unstable.nodePackages) prettier;
+    }
+
+    ###################################################
+    # Languages:
+    {
+      inherit (pkgs.stable) nodejs-16_x;
+      inherit (pkgs.unstable.nodePackages) typescript;
+      inherit (pkgs.stable) purescript;
+    }
+
+    ###################################################
+    # Command line tools:
+    { inherit (pkgs.stable) gitFull gitflow;
+      inherit (pkgs.unstable.nodePackages) parcel-bundler;
+    }
+
+    ###################################################
+    # Language servers:
+    {
+      inherit (pkgs.stable) dhall-lsp-server;
+      inherit (pkgs.unstable.nodePackages)
+        bash-language-server
+        typescript-language-server
+        purescript-language-server
+        vscode-json-languageserver-bin
+        vscode-html-languageserver-bin
+        yaml-language-server;
+    }
+
+    ###################################################
+    # Package managers:
+    {
+      inherit (pkgs.stable) spago pulp;
+      inherit (pkgs.unstable.nodePackages) bower;
+    }
+  ];
+}

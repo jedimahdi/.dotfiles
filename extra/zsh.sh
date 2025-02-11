68f659db1f5e740d4eee4319008d113eaef5a0d9
Mahdi Seyedan <mahdi.se@yahoo.com>
2025-02-11 04:03:49 +0330

    updates and add fuzzel and cliphist to hyprland

---
+++ extra/zsh.sh
@@ -0,0 +1,6 @@
+
+mkdir -p ~/.local/share/zsh/plugins
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.local/share/zsh/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.local/share/zsh/plugins/zsh-syntax-highlighting
